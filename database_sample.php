00e97814432d5f0dbf7e1d55147db973c942dcb1
Tuanyk <nguyenhuuhoangtuan.ftu@gmail.com>
2023-06-25 23:22:35 +0700

    font

--- database_sample.php
+++ database_sample.php
@@ -5,4 +5,8 @@ $leodb = new \MongoDB\Client("mongodb://username:password@host:port");
 
 $database_name = 'database_name';
 $collection_name = 'collection_name';
-$site_lang = 'en_vi';
+$site = [
+    'localhost:8093' => 'en_vi',
+];
+
+$site_lang = $site[$_SERVER['HTTP_HOST']];
--- view/document.php
+++ view/document.php
@@ -1,1 +1,7 @@
+<?php include __DIR__.'/template-parts/header.php'; ?>
+
+
+
 <?= $document["translated_html_$site_lang"]; ?>
+
+<?php include __DIR__.'/template-parts/footer.php'; ?>
