64055fe82380b9e7a3b8625f836009090800cc61
Tuanyk <nguyenhuuhoangtuan.ftu@gmail.com>
2023-06-26 17:59:23 +0700

    fix alittle

--- view/document.php
+++ view/document.php
@@ -3,7 +3,21 @@
 <div class="container mt-3 mb-3 border border-secondary-subtle p-3 ml-3 mr-3">
     <h1><?= $document['title']; ?></h1>
     <h2 class="subtitle"><?= $document["title_$site_lang"]; ?></h2>
-    <?= $document["translated_html_$site_lang"]; ?>
+    <div class="content">
+        <?php 
+            $html = $document["translated_html_$site_lang"]; 
+            $dom = new DOMDocument();
+            $dom->loadHTML('<?xml encoding="UTF-8">' . $html);
+            $image_tags = $dom->getElementsByTagName('img');
+            foreach ($image_tags as $image_tag) {
+                $src = $image_tag->getAttribute('src');
+                $new_img_src = '/images/' . $document['_id'] . '_' . md5($src) . '.webp';
+                $image_tag->setAttribute('src', $new_img_src);
+            }
+            $html = $dom->saveHTML();
+            echo $html;
+        ?>
+    </div>
 </div>
 
 <?php include __DIR__.'/template-parts/footer.php'; ?>
