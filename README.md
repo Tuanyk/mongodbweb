526123ab1bb27ec8cc2441a017e587e215b86a21
Tuanyk <nguyenhuuhoangtuan.ftu@gmail.com>
2023-06-28 08:58:15 +0700

    fix database

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# MyWebsite
+
+You can see it here: [vanchuyenqt.com](vanchuyenqt.com)
+
 First, rename database_sample.php to database.php, and replace your mongodb login string to match your setup.
 
 # Database structure
--- database_sample.php
+++ database_sample.php
@@ -5,15 +5,62 @@ $leodb = new \MongoDB\Client("mongodb://username:password@host:port");
 
 $database_name = 'database_name';
 $collection_name = 'collection_name';
-$site = [
-    'localhost:8093' => [
-        'lang'=>'en_vi',
-        'title'=>'HomePage Title',
+$site_metadata = [
+    'vi' => [
+        'title'=>'Vi HomePage Title',
         'description'=>'HomePage Description',
-
+    ],
+    'it' => [
+        'title'=>'It HomePage Title',
+        'description'=>'HomePage Description',
+    ],
+    'fr' => [
+        'title'=>'Fr HomePage Title',
+        'description'=>'HomePage Description',
+    ],
+    'de' => [
+        'title'=>'De HomePage Title',
+        'description'=>'HomePage Description',
+    ],
+    'ja' => [
+        'title'=>'Ja HomePage Title',
+        'description'=>'HomePage Description',
+    ],
+    'pt' => [
+        'title'=>'Pt HomePage Title',
+        'description'=>'HomePage Description',
+    ],
+    'fi' => [
+        'title'=>'Fi HomePage Title',
+        'description'=>'HomePage Description',
+    ],
+    'ar' => [
+        'title'=>'Ar HomePage Title',
+        'description'=>'HomePage Description',
+    ],
+    'es' => [
+        'title'=>'Es HomePage Title',
+        'description'=>'HomePage Description',
+    ],
+    'ko' => [
+        'title'=>'Ko HomePage Title',
+        'description'=>'HomePage Description',
     ],
 ];
 
-$site_lang = $site[$_SERVER['HTTP_HOST']]['lang'];
-$site_title = $site[$_SERVER['HTTP_HOST']]['title'];
-$site_desc = $site[$_SERVER['HTTP_HOST']]['description'];
+
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+    // HTTPS request
+    $scheme = 'https';
+} else {
+    // HTTP request
+    $scheme = 'http';
+}
+
+$site_url = $scheme.'://'.$_SERVER['HTTP_HOST'];
+
+$site_info = [
+    'metadata' => $site_metadata,
+    'site_url' => $site_url,
+    'site_name' => 'VanChuyenQT',
+];
