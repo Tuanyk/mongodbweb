d5fb04b8c251a4edd897f132f1bdebc8a05e174d
Tuanyk <nguyenhuuhoangtuan.ftu@gmail.com>
2023-06-28 09:22:48 +0700

    add categories

--- view/document.php
+++ view/document.php
@@ -21,8 +21,9 @@
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="/">Homepage</a></li>
-
-                        <li class="breadcrumb-item active" aria-current="page">Web Design</li>
+                        <?php foreach ($document['categories'] as $category): ?>
+                            <li class="breadcrumb-item active"><?= $category; ?></li>
+                        <?php endforeach; ?>
                     </ol>
                 </nav>
 
